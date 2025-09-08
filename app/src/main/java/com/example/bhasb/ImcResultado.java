dc98873fa1d8b88fea3a5781cb484d5609c39e7c
joaopedropitzz <joao.p2007@aluno.ifsc.edu.br>
2025-09-08 09:11:48 -0300

    calculadora de gordo

--- app/src/main/java/com/example/bhasb/ImcResultado.java
+++ app/src/main/java/com/example/bhasb/ImcResultado.java
@@ -1,6 +1,5 @@
 package com.example.bhasb;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,19 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    
-    Button button;
+public class ImcResultado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        button.setOnClickListener(v->{
-            Intent intent= new Intent(this, ImcResultado.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_imc_resultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
