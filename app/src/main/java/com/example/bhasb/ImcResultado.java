b6936b53225262d0cac8452b992d6a6df8579923
joaopedropitzz <joao.p2007@aluno.ifsc.edu.br>
2025-09-15 09:15:03 -0300

    calculadora imc

--- app/src/main/java/com/example/bhasb/ImcResultado.java
+++ app/src/main/java/com/example/bhasb/ImcResultado.java
@@ -1,6 +1,9 @@
 package com.example.bhasb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ImcResultado extends AppCompatActivity {
+    TextView tvResultado;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imc_resultado);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tvResultado=findViewById(R.id.textView2);
+        imageView=findViewById(R.id.imageViewPerfil);
+        //recuperando a intenção
+        Intent i=getIntent();
+        Bundle b=i.getExtras();
+        Double peso =b.getDouble("peso");
+        Double altura =b.getDouble("altura");
+        Double imc = peso/(altura*altura);
+        tvResultado.setText(Double.toString(imc));
+        imageView.setImageResource(R.drawable.perfil);
+
+        if(imc<18.9){imageView.setImageResource(R.drawable.abaixopeso);}
+        if(imc>=18.9 && imc<25){imageView.setImageResource(R.drawable.normal);}
+        if(imc>=25 && imc<30){imageView.setImageResource(R.drawable.normal);}
+        if(imc>=30 && imc<35){imageView.setImageResource(R.drawable.normal);}
+
+
     }
 }
--- app/src/main/java/com/example/bhasb/MainActivity.java
+++ app/src/main/java/com/example/bhasb/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bhasb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     
     Button button;
+    EditText edPeso,edAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button4);
+        edPeso = findViewById(R.id.edPeso);
+        edAltura = findViewById(R.id.edAltura);
         button.setOnClickListener(v->{
             Intent intent= new Intent(this, ImcResultado.class);
+            Bundle bundle =new Bundle();
+            Double peso = Double.parseDouble(edPeso.getText().toString());
+            Double altura = Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("peso",peso);
+            bundle.putDouble("altura",altura);
+            intent.putExtras(bundle);
             startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
