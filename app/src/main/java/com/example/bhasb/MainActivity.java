4f931d1ac985294c2b365fd375ce8c19228f74b5
Vitor Hugo Prange <vitor.p2007@aluno.ifsc.edu.br>
2025-09-29 09:06:57 -0300

    aula 29/09

--- app/src/main/java/com/example/bhasb/MainActivity.java
+++ app/src/main/java/com/example/bhasb/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bhasb;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    String [] nomes = new String[]{"Cesar", "José","Romulo","Clara","Samanta","Camila","Gustavo","Bilu"};
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listview);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Toast.makeText(getApplicationContext(),nomes[position],Toast.LENGTH_LONG).show();
+        });
+
+
     }
 }
