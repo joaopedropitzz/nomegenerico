99bd4ecb21b3d86296c9bafb29fe7430064f76f3
joaopedropitzz <joao.p2007@aluno.ifsc.edu.br>
2025-10-06 09:10:04 -0300

    06/10

--- app/src/main/java/com/example/bhasb/MainActivity.java
+++ app/src/main/java/com/example/bhasb/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bhasb;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,29 +13,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    String [] nomes = new String[]{"Cesar", "José","Romulo","Clara","Samanta","Camila","Gustavo","Bilu"};
+
     ListView listView;
+    Button button;
+    EditText editTextText;
+    ArrayList<String> nomes;
+    String i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listview);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        button = findViewById(R.id.button);
+        editTextText = findViewById(R.id.editTextText);
+        nomes = new ArrayList<String>();
+
 
         ArrayAdapter<String> adapter= new ArrayAdapter<>(getApplicationContext(),
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
                 nomes);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            Toast.makeText(getApplicationContext(),nomes[position],Toast.LENGTH_LONG).show();
+        button.setOnClickListener(v ->{
+            nomes.add(editTextText.getText().toString());
+            adapter.notifyDataSetChanged();
         });
+
+        listView.setOnItemLongClickListener((parent, view, position, id)->{
+            nomes.remove(position);
+            adapter.notifyDataSetChanged();
+            return true;
+        });
+
+
 
 
     }
