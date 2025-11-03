eba4b6f97b485a817554e33807fce889f5ffac02
joaopedropitzz <joao.p2007@aluno.ifsc.edu.br>
2025-11-03 09:10:47 -0300

    banco de dados

--- app/src/main/java/com/example/bhasb/MainActivity.java
+++ app/src/main/java/com/example/bhasb/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bhasb;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    SQLiteDatabase db;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        db = openOrCreateDatabase("app_database", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "titulo VARCHAR, texto TEXT)");
+        ContentValues values = new ContentValues();
+        values.put("titulo", "Nota de Exemplo");
+        values.put("texto", "Este é o texto da nota de exemplo. Chega natal!");
+        db.insert("notas", null, values);
+        b = findViewById(R.id.button);
+        b.setOnClickListener(v ->{
+            EditText editText = findViewById(R.id.editTextText);
+            String texto = editText.getText().toString();
+            ContentValues cv = new ContentValues();
+            cv.put("titulo", "Nota do Usuário");
+            cv.put("texto", texto);
+            db.insert("notas", null, cv);
+            Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
         });
     }
 }
