99b6f5ad1a58bc13a6b23e931fdc0db50e1caf33
joaopedropitzz <joao.p2007@aluno.ifsc.edu.br>
2025-11-10 09:08:22 -0300

    oreia seca

--- app/src/main/java/com/example/bhasb/MainActivity.java
+++ app/src/main/java/com/example/bhasb/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.bhasb;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,14 +16,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.bhasb.R;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
-    Button b;
+    Button button;
+    EditText editText;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.buttonSalvar);
+        editText = findViewById(R.id.editTextText);
+        listView = findViewById(R.id.listView);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -28,21 +42,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
         db = openOrCreateDatabase("app_database", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                "titulo VARCHAR, texto TEXT)");
-        ContentValues values = new ContentValues();
-        values.put("titulo", "Nota de Exemplo");
-        values.put("texto", "Este é o texto da nota de exemplo. Chega natal!");
-        db.insert("notas", null, values);
-        b = findViewById(R.id.button);
-        b.setOnClickListener(v ->{
-            EditText editText = findViewById(R.id.editTextText);
-            String texto = editText.getText().toString();
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " titulo VARCHAR, txt TEXT);");
+
+        carregarListagem();
+
+        button.setOnClickListener(v -> {
+            String titulo = editText.getText().toString();
+
             ContentValues cv = new ContentValues();
-            cv.put("titulo", "Nota do Usuário");
-            cv.put("texto", texto);
+            cv.put("titulo", titulo);
+
             db.insert("notas", null, cv);
-            Toast.makeText("Nota salva com sucesso!", Toast.LENGTH_SHORT).show();
+
+            carregarListagem();
         });
+    }
+
+    private void carregarListagem() {
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+        cursor.moveToFirst();
+
+        ArrayList<String> titulos = new ArrayList<String>();
+
+        while (!cursor.isAfterLast()) {
+            titulos.add(cursor.getString(cursor.getColumnIndex("titulo")));
+            cursor.moveToNext();
+        }
+
+        ArrayAdapter<String> titulosAdapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                titulos
+        );
+
+        listView.setAdapter(titulosAdapter);
+
     }
 }
